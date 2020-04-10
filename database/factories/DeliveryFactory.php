76a58ef0baa90747cd62e19b672d40c758f4199f
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2020-04-10 16:24:58 +0100

    Adds delivery resources

---
+++ database/factories/DeliveryFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Delivery;
+use App\Models\User;
+use App\Models\Pickup;
+use Faker\Generator as Faker;
+
+$factory->define(Delivery::class, function (Faker $faker) {
+    return [
+        //
+        'user_id' => function () {
+            return factory(User::class)->create()->id;
+        },
+        'pickup_id' => function () {
+            return factory(Pickup::class)->create()->id;
+        },
+        'notes' => $faker->paragraph(),
+    ];
+});
