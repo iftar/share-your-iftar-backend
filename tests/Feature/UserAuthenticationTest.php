c1cb15c435d46fbc584fb7346b2e6f648349744f
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2020-04-18 16:27:16 +0100

    Fix all tests

--- tests/Feature/UserAuthenticationTest.php
+++ tests/Feature/UserAuthenticationTest.php
@@ -39,32 +39,6 @@ class UserAuthenticationTest extends TestCase
                 ]]]);
     }
 
-    public function testRegisterCharityUser()
-    {
-        $password = $this->faker->password;
-        $postData = [
-            'email'             => $this->faker->email,
-            'password'          => $password,
-            'confirm'           => $password,
-            'first_name'        => $this->faker->firstName,
-            'last_name'         => $this->faker->firstName,
-            'type'              => 'charity',
-        ];
-        $response = $this->postJson('/api/register', $postData);
-
-        $response
-            ->assertStatus(200)
-            ->assertJson([
-                "status" => "success",
-                "data" => [ "user" => [
-                    'email'             => $postData['email'],
-                    'first_name'        => $postData['first_name'],
-                    'last_name'         => $postData['last_name'],
-                    'type'              => $postData['type'],
-                    'status'            => 'approved',
-                ]]]);
-    }
-
     public function testRegisterThenLoginWithoutVerifying()
     {
         // register
