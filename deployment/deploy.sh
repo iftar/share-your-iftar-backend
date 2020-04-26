1d9e57aa4a050ef13469fda5b1db1c08988046d7
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2020-04-26 01:14:07 +0100

    updates

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -21,14 +21,17 @@ sudo chown -R $USER:$USER /home/ubuntu/.composer/
 sudo chmod -R 775 /home/ubuntu/.composer/cache/
 
 sudo mkdir /var/www/shareiftar.org/storage
+sudo mkdir /var/www/shareiftar.org/storage/framework
+sudo mkdir /var/www/shareiftar.org/storage/framework/cache
+sudo mkdir /var/www/shareiftar.org/storage/framework/cache/data
 sudo chown ubuntu:www-data -R /var/www/shareiftar.org/storage
 sudo chmod -R 775 /var/www/shareiftar.org/storage
 
-sudo add-apt-repository ppa:ondrej/php
-sudo apt update
-sudo apt upgrade -y
+#sudo add-apt-repository ppa:ondrej/php
+#sudo apt update
+#sudo apt upgrade -y
 dpkg -l | grep php | tee packages.txt
-sudo apt install php7.4 php7.4-common php7.4-cli php7.4-fpm php7.4-xml php7.4-mbstring php7.4-curl php7.4-pgsql php7.4-json php-json-schema php-cli-prompt php7.4-common php-composer-semver php-composer-semver php-composer-spdx-licenses php-symfony-console php-symfony-filesystem php-symfony-finder php-symfony-process -y
-sudo apt install zip unzip php7.4-zip -y 
+sudo apt install php7.2 php7.2-common php7.2-cli php7.2-fpm php7.2-xml php7.2-mbstring php7.2-curl php7.2-pgsql php7.2-json php-json-schema php-cli-prompt php7.2-common php-composer-semver php-composer-semver php-composer-spdx-licenses php-symfony-console php-symfony-filesystem php-symfony-finder php-symfony-process -y
+sudo apt install zip unzip php7.2-zip -y 
 sudo apt install php-bcmath -y
 sudo apt install php-gd -y
