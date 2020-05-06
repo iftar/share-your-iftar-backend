5e1421ddd9b1fe4b53c48f2f8d5b123dc1542f1e
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2020-05-06 03:34:28 +0100

    Add caching to todays orders api

--- app/Http/Controllers/API/Admin/OrderController.php
+++ app/Http/Controllers/API/Admin/OrderController.php
@@ -5,16 +5,27 @@ namespace App\Http\Controllers\API\Admin;
 use App\Http\Controllers\Controller;
 use App\Services\Admin\OrderService;
 use App\Http\Requests\API\Admin\AuthenticatedRequest;
+use Illuminate\Support\Facades\Cache;
 
 class OrderController extends Controller
 {
     public function index(AuthenticatedRequest $request, OrderService $orderService)
     {
+        $cache_key = 'admin_todays_orders';
+
+        $data = Cache::get($cache_key);
+
+        if ( ! $data ) {
+            $data = [
+                'orders' => $orderService->getOrdersToday(),
+                'last_updated' => now('Europe/London')->format('H:m a'),
+            ];
+            Cache::put($cache_key, $data, 300); // 5 mins
+        }
+
         return response()->json([
             'status' => 'success',
-            'data'   => [
-                'orders' => $orderService->getOrdersToday()
-            ]
+            'data'   => $data
         ]);
     }
 }
